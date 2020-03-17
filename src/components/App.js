88c7fd638c188ef540f13ca5ff583b79400ad208
Lira, Moises <jesuslira87@gmail.com>
2020-03-17 14:08:05 -0600

    implemented API call with normal use of then to work with response

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
 	onSearchSubmit(term) {
-		console.log(term);
+		axios
+			.get("https://api.unsplash.com/search/photos", {
+				params: { query: term },
+				headers: {
+					Authorization:
+						"Client-ID wJeGWkfycKuFI0fF31pOvlpcSYkds3iTfaZzEzMbKBA",
+				},
+			})
+			.then(response => {
+				console.log(response.data.results);
+			});
 	}
 	render() {
 		return (
