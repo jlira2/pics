b1bce7c591eaaa7c4c2ba21d70f9d366d94ed9ad
Lira, Moises <jesuslira87@gmail.com>
2020-03-17 15:42:52 -0600

    started image list

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
+import ImageList from "./ImageList";
+import unsplash from "../api/unsplash";
 
 class App extends React.Component {
 	state = { images: [] };
@@ -18,7 +19,7 @@ class App extends React.Component {
 			<div>
 				<div className='ui container' style={{ marginTop: "10px" }}>
 					<SearchBar onSubmit={this.onSearchSubmit} />
-					Found: {this.state.images.length} images
+					<ImageList images={this.state.images} />
 				</div>
 			</div>
 		);
