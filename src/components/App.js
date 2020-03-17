9c7e55c3ac839231f06a9adb9ae0064d9ecf229f
Lira, Moises <jesuslira87@gmail.com>
2020-03-17 14:10:40 -0600

    implemented API call with new use of await and async to work with response

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,14 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-	onSearchSubmit(term) {
-		axios
-			.get("https://api.unsplash.com/search/photos", {
-				params: { query: term },
-				headers: {
-					Authorization:
-						"Client-ID wJeGWkfycKuFI0fF31pOvlpcSYkds3iTfaZzEzMbKBA",
-				},
-			})
-			.then(response => {
-				console.log(response.data.results);
-			});
+	async onSearchSubmit(term) {
+		const response = await axios.get("https://api.unsplash.com/search/photos", {
+			params: { query: term },
+			headers: {
+				Authorization: "Client-ID wJeGWkfycKuFI0fF31pOvlpcSYkds3iTfaZzEzMbKBA",
+			},
+		});
+		console.log(response.data.results);
 	}
 	render() {
 		return (
