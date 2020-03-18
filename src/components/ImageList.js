4047f02ef617c09a22ce1d86c6e3dad83018ec73
Lira, Moises <jesuslira87@gmail.com>
2020-03-17 18:10:04 -0600

    showed images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,12 @@
 import React from "react";
 
 const ImageList = props => {
-	console.log(props.images);
-	return <div>ImageList</div>;
+	const images = props.images.map(image => {
+		return (
+			<img key={image.id} alt={image.description} src={image.urls.regular} />
+		);
+	});
+	return <div>{images}</div>;
 };
 
 export default ImageList;
